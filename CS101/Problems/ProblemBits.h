8968ce7cf16015fa546f324c5f36244a6353036c
Benito Gangoso Rodriguez <benito.gangosorodriguez@gmail.com>
2017-07-04 21:10:20 +0200

    !T Adding some descriptions to "Problems"

--- CS101/Problems/ProblemBits.h
+++ CS101/Problems/ProblemBits.h
@@ -2,7 +2,13 @@
 
 namespace Problems
 {
-	int Insert(int M, int N, int i, int j);
+	// Description:
+	// Given N and M integers, insert M bits into [i,j] bits
+	// N is supposed to have as many bits as j-i
+	int Insert(int N, int M, int i, int j);
 
+	// Description:
+	// Given A and B integers, calculate how many bits we have to swap
+	// to make A == B
 	int BitSwapRequired(int A, int B);
 }
